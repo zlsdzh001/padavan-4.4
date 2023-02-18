c1ab97e2743b8f4ac9549181446be72afdd1ec0b
xychenxy <chenxinyi1978@hotmail.com>
2023-02-18 10:13:13 +0800

    remove nr_uninterruptible

--- trunk/linux-4.4.x/kernel/sched/loadavg.c
+++ trunk/linux-4.4.x/kernel/sched/loadavg.c
@@ -83,7 +83,7 @@ long calc_load_fold_active(struct rq *this_rq)
 	long nr_active, delta = 0;
 
 	nr_active = this_rq->nr_running;
-	nr_active += (long)this_rq->nr_uninterruptible;
+	//nr_active += (long)this_rq->nr_uninterruptible;
 
 	if (nr_active != this_rq->calc_load_active) {
 		delta = nr_active - this_rq->calc_load_active;
